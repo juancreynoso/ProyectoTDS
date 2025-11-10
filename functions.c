592b8aac5b32fc12df53798eb7a46bf08073fd6f
juancreynoso <juancreynoso11@gmail.com>
2025-11-10 20:21:43 -0300

    add assert functions for testing

--- functions.c
+++ functions.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "functions.h"
 
 void print(int n) {
@@ -11,3 +12,19 @@ int get_int() {
 
     return x;
 }
+
+void assert(int result, int expected) {
+    const char* GREEN = "\033[1;32m";
+    const char* RED   = "\033[1;31m";
+    const char* RESET = "\033[0m";
+
+    if (result != expected) {
+        printf("%s[FAIL]%s Resultado esperado: %d, obtenido: %d\n",
+               RED, RESET, expected, result);
+        exit(1);
+    } else {
+        printf("%s[OK]%s Resultado esperado: %d, obtenido: %d\n",
+               GREEN, RESET, result, expected);
+    }
+}
+
